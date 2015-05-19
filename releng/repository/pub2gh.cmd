a3ca1a82598d14b0d6f955024e142b34d72185f0
Marco Lehmann-Mörz <mlehmannm@googlemail.com>
2015-05-19 13:03:30 +0200

    fixed script to publish p2-repository to gh-pages

--- releng/repository/pub2gh.cmd
+++ releng/repository/pub2gh.cmd
@@ -4,6 +4,9 @@
 @echo off
 @if not "%ECHO%" == "" echo %ECHO% 
 
+@rem check if email has been provided
+if "%1" == "" goto noemail
+
 @rem prepare gh-pages clone directory
 set "CLONE_DIR=%~dp0\tmp"
 set "CLONE_DIR=%CLONE_DIR:\\=\%"
@@ -13,7 +16,7 @@ md %CLONE_DIR% >nul
 @rem clone gh-pages
 call git branch -q -D gh-pages
 call git fetch -q origin gh-pages:gh-pages
-call git clone -q -l -s -b gh-pages .. "%CLONE_DIR%\gh-pages"
+call git clone -q -l -s -b gh-pages ../.. "%CLONE_DIR%\gh-pages"
 cd "%CLONE_DIR%\gh-pages"
 
 @rem cleanup old update-site
@@ -25,5 +28,14 @@ robocopy /E /NP "%~dp0\root-files" .\ >nul
 
 @rem commit
 call git add .
+call git config --local --add user.email %1
 call git commit -q -m "new release published"
 call git push -q
+echo git push origin gh-pages:gh-pages
+goto end
+
+:noemail
+echo %0 ^<email-address^>
+
+:end
+@endlocal
