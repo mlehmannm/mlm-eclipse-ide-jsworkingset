c8159bc58cb7b459c79ba6642f5ad33bac50e3db
Marco Lehmann-Mörz <mlehmannm@googlemail.com>
2015-05-20 12:06:44 +0200

    prepare new module 'pde'

---
+++ bundles/mlm.eclipse.ide.jsworkingset/resources/pde.js
@@ -0,0 +1,31 @@
+// pde.js
+
+var pde = {};
+
+pde.isFeature = function(pProject) {
+
+	// TODO
+	return false;
+
+}
+
+pde.isPlugin = function(pProject) {
+
+	// TODO
+	return false;
+
+}
+
+pde.isFragment = function(pProject) {
+
+	// TODO
+	return false;
+
+}
+
+var module = {
+	name : 'pde',
+	exports : {
+		pde : pde,
+	},
+};
