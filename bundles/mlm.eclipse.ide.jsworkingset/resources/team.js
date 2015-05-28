2d11614a6291efb590fd1d8f2073603423f304f7
Marco Lehmann-Mörz <mlehmannm@googlemail.com>
2015-05-28 06:45:13 +0200

    simple team support

---
+++ bundles/mlm.eclipse.ide.jsworkingset/resources/team.js
@@ -0,0 +1,17 @@
+// team.js
+
+var team = {};
+
+team.isShared = function(pProject) {
+
+	var RepositoryProvider = org.eclipse.team.core.RepositoryProvider;
+	return RepositoryProvider.getProvider(pProject) !== null;
+
+}
+
+var module = {
+	name : 'team',
+	exports : {
+		team : team,
+	},
+};
